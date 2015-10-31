3b29710e1011d60991de85e0ea7e1729d13206c4
dxeware <dxeware@gmail.com>
2015-10-31 08:20:58 -0700

    Removed done from tests since all tests are synchronous

--- frequency.Spec.js
+++ frequency.Spec.js
@@ -4,44 +4,40 @@ var expect = require('chai').expect;
 
 describe('frequency method', function() {
 
-  it('should return \'a\' as most frequent letter', function(done) {
+  it('should return \'a\' as most frequent letter', function() {
     var array = ['caT', 'MAP', 'Laura', 'z'];
     var char;
 
     char = array.myFrequency();
 
     expect(char).to.equal('a');
-    done();
   });
 
-  it('should not double count letters in same word', function(done) {
+  it('should not double count letters in same word', function() {
     var array = ['ladder', 'moused', 'sit', 'set'];
     var char;
 
     char = array.myFrequency();
 
     expect(char).to.equal('s');
-    done();
   });
 
-  it('should return highest letter \'s\' when most frequent letter when letter frequency is tie', function(done) {
+  it('should return highest letter \'s\' when most frequent letter when letter frequency is tie', function() {
     var array = ['shell', 'shawl', 'cat'];
     var char;
 
     char = array.myFrequency();
 
     expect(char).to.equal('s');
-    done();
   });
 
-  it('should ignore anything other than [a-z][A-Z]', function(done) {
+  it('should ignore anything other than [a-z][A-Z]', function() {
     var array = ['cat0', 'house1', 'bill1'];
     var char;
 
     char = array.myFrequency();
 
     expect(char).to.equal('u');
-    done();
   });
 
 });
